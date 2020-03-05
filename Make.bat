af80d3de89de732fc6b8f6a9821f1f9ac8c7f9db
TheDeadFish <michaelstamper1@gmail.com>
2020-03-05 01:42:19 +0000

    1. converted build to cmake 2. make.bat: libmerge into libexshit.a

--- Make.bat
+++ Make.bat
@@ -1,15 +1,5 @@
-:: build library
-call egcc.bat
-pushd bin\obj
-gcc %CCFLAGS2% ..\..\*.cc -c
-popd
-ar -rcs  bin\libexshit.a bin\obj\*.o
-
-:: build tools
-gcc %CCFLAGS2% app\icon-shrink.cc -o bin\icon-shrink.exe bin\libexshit.a -lstdshit
-gcc %CCFLAGS2% app\lang-kill.cc -o bin\lang-kill.exe bin\libexshit.a -lstdshit
-gcc %CCFLAGS2% app\res-dump.cc -o bin\res-dump.exe bin\libexshit.a -lstdshit
+@call cmake_gcc.bat
 
 :: install the library
-copy /Y *.h %PROGRAMS%\local\include
-copy /Y bin\*.a %PROGRAMS%\local\lib32
+@libmerge %PROGRAMS%\local\lib32\libexshit.a  bin\libres.a
+@copy /Y *.h %PROGRAMS%\local\include
